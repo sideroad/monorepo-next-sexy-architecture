a31669edb8a0fdc71ef7db22f2a5a388459df436
sideroad <sideroad.jp@gmail.com>
2019-10-06 20:55:02 +0900

    Add nextbook instead of storybook

---
+++ apps/hello-world/components/nextbook.tsx
@@ -0,0 +1,28 @@
+/* eslint-disable react/display-name */
+import React from 'react';
+import Search from './search';
+import User from './user';
+
+const nextBook = {
+  title: 'Hello World',
+  books: [
+    {
+      name: 'Search',
+      desc: 'Be able to search github user by ID',
+      component: <Search onSubmit={() => console.log('submitted')} />
+    },
+    {
+      name: 'User',
+      desc: 'Show user profile',
+      component: (
+        <User
+          login="sideroad"
+          avatar_url="https://avatars2.githubusercontent.com/u/411486?v=4"
+          html_url="https://github.com/sideroad"
+        />
+      )
+    }
+  ]
+};
+
+export default nextBook;
